a17941b82bdbeef2f82e8681e2118b11c0e81c1a
sushi <officialaxos7@gmail.com>
2024-02-27 00:30:49 -0800

    add traceback error handler to lua

--- src/main.c
+++ src/main.c
@@ -6,13 +6,27 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
+// callback used when lua encounters an error
+// that prints a trace
+static int traceback(lua_State *L) {
+    lua_getfield(L, LUA_GLOBALSINDEX, "debug");
+    lua_getfield(L, -1, "traceback");
+    lua_pushvalue(L, 1);
+    lua_pushinteger(L, 2);
+    lua_call(L, 2, 1);
+    return 1;
+}
+
 int main(){
 	// initialize lua
 	
 	lua_State* L = lua_open();
 	luaL_openlibs(L); // load std lua libs
 	
-	if (luaL_loadfile(L, "src/main.lua") || lua_pcall(L, 0, 0, 0)) {
+	// push traceback function onto the stack and reference it as the error handler
+	// (the last argument to lua_pcall)
+	lua_pushcfunction(L, traceback); 
+	if (luaL_loadfile(L, "src/main.lua") || lua_pcall(L, 0, 0, lua_gettop(L) - 1)) {
 		printf("%s\n", lua_tostring(L, -1));
 		return 1;
 	}
