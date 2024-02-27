dc492d4113d4dc267c904fc9ba1ac84b7322e765
sushi <officialaxos7@gmail.com>
2024-02-27 00:31:19 -0800

    work on game loopo

--- src/game.lua
+++ src/game.lua
@@ -8,11 +8,22 @@ local log = function(...)
 	io.write("game: ", ..., "\n")
 end
 
+local co = require "coroutine"
+
 local cards = require "cards"
+local Player = require "player"
 local raylib = require "raylib";
+
+local dbg = require "debugger"
 
 -- primary game state table
 local game = {}
+
+local map_linear_range = function(in_start, in_end, out_start, out_end, x)
+	local ind = in_end - in_start
+	local outd = out_end - out_start
+	return out_start + outd * ((x - in_start) / ind)
+end
 
 game.
 init = function(self, n_players)
@@ -28,10 +39,9 @@ init = function(self, n_players)
 	-- create each player table
 	self.players = {}
 	for _=1,n_players do
-		table.insert(self.players, {
-			level = 1;
-			cards = {};
-		})
+		local o = {}
+		setmetatable(o, Player)
+		table.insert(self.players, o)
 	end
 
 	log("creating door and treasure decks")
@@ -44,16 +54,84 @@ init = function(self, n_players)
 		-- set of monsters active in combat
 		monsters = {};
 	}
+
+	self.current_animation = nil
 end
+
+-- defs of different kinds of animations 
+local animations = {
+	turn_start = {
+		slide_in_time = 0.25; -- seconds
+		still_time = 1;
+		slide_out_time = 0.25;
+
+		init = function(self)
+			local text = "Turn Start"
+			local time = 0;
+			local text_width = raylib.MeasureText(text, 20)
+
+			local out = co.create(function()
+				dbg()
+				local x
+				while true do
+					log("animation")
+					time = time + raylib.GetFrameTime()
+					x = map_linear_range(0, self.slide_in_time, 0, (text_width - raylib.GetRenderWidth) / 2, time)
+
+					raylib.DrawText(text, x, 0, 11, raylib.WHITE)
+					co.yield()
+				end
+			end)
+			dbg()
+			return out
+		end
+	}
+}
+
+local wait_for_animations = function(self)
+	while self.current_animation do
+		co.yield()
+	end
+end
+
+local update_coroutine = co.create(function(self)
+	::turn_start::
+	do
+		log("--( turn start )--")
+		self.current_animation = animations.turn_start:init()
+		dbg()
+		wait_for_animations(self)
+
+		-- get the active player 
+		local active_player = self.players[#self.players % self.turn + 1]
+	end
+
+	goto turn_start
+end)
 
 game.
 update = function(self)
+	-- log(" --- ** update start ** ---")
 
+	local all_good, message = co.resume(update_coroutine, self)
+
+	if not all_good then
+		log("coroutine error: "..message)
+	end
+
+	-- log(" *** -- update end -- *** ")
 end
 
 game.
 draw = function(self)
 	raylib.DrawFPS(0, 0);
+
+	if self.current_animation then
+		local not_done = co.resume(self.current_animation)
+		if not not_done then
+			self.current_animation = nil
+		end
+	end
 end
 
 return game;
